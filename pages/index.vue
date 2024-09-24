2fb916ba8919f50d87f423e604522e026044a457
asnygn <asnygn@gmail.com>
2024-09-24 20:52:41 +0630

    Update home page

--- pages/index.vue
+++ pages/index.vue
@@ -142,6 +142,122 @@
         </ul>
       </Container>
     </section>
+    <section class="why">
+      <Container>
+        <div class="why__content">
+          <div class="why__title">
+            <p>Why Us?</p>
+          </div>
+          <div class="why__list">
+            <div class="why-item">
+              <div class="why-item__image">
+                <img src="/assets/images/checked.png" alt="" />
+              </div>
+              <div class="why-item__content">
+                <div class="why-item__title">
+                  <p>Best Brands</p>
+                </div>
+                <div class="why-item__description">
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Quisque consectetur elementum velit nec feugiat.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="why-item">
+              <div class="why-item__image">
+                <img src="/assets/images/checked.png" alt="" />
+              </div>
+              <div class="why-item__content">
+                <div class="why-item__title">
+                  <p>Best Brands</p>
+                </div>
+                <div class="why-item__description">
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Quisque consectetur elementum velit nec feugiat.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="why-item">
+              <div class="why-item__image">
+                <img src="/assets/images/checked.png" alt="" />
+              </div>
+              <div class="why-item__content">
+                <div class="why-item__title">
+                  <p>Best Brands</p>
+                </div>
+                <div class="why-item__description">
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Quisque consectetur elementum velit nec feugiat.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </section>
+    <section class="news">
+      <Container>
+        <div class="news__title">
+          <p>Update News</p>
+        </div>
+        <div class="news__list">
+          <div class="news-item">
+            <div class="news-item__image">
+              <img src="/assets/images/news-1.jpg" alt="" />
+            </div>
+            <div class="news-item__title">
+              Pellentesque consequat nulla et lorem
+            </div>
+            <div class="news-item__description">
+              Vitae sagittis erat cursus. Sed nec fringilla turpis. Sed et
+              mauris laoreet, malesuada enim malesuada, finibus nibh. Sed sit
+              amet risus fringilla
+            </div>
+            <div class="news-item__link">
+              <NuxtLink to="/">Read more</NuxtLink>
+            </div>
+          </div>
+          <div class="news-item">
+            <div class="news-item__image">
+              <img src="/assets/images/news-2.jpg" alt="" />
+            </div>
+            <div class="news-item__title">
+              Pellentesque consequat nulla et lorem
+            </div>
+            <div class="news-item__description">
+              Vitae sagittis erat cursus. Sed nec fringilla turpis. Sed et
+              mauris laoreet, malesuada enim malesuada, finibus nibh. Sed sit
+              amet risus fringilla
+            </div>
+            <div class="news-item__link">
+              <NuxtLink to="/">Read more</NuxtLink>
+            </div>
+          </div>
+          <div class="news-item">
+            <div class="news-item__image">
+              <img src="/assets/images/news-3.jpg" alt="" />
+            </div>
+            <div class="news-item__title">
+              Pellentesque consequat nulla et lorem
+            </div>
+            <div class="news-item__description">
+              Vitae sagittis erat cursus. Sed nec fringilla turpis. Sed et
+              mauris laoreet, malesuada enim malesuada, finibus nibh. Sed sit
+              amet risus fringilla
+            </div>
+            <div class="news-item__link">
+              <NuxtLink to="/">Read more</NuxtLink>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </section>
   </main>
 </template>
 
@@ -165,7 +281,10 @@ main {
 }
 /* Services */
 .services {
-  margin-bottom: 80px;
+  position: relative;
+  padding-top: 40px;
+  padding-bottom: 80px;
+  background-color: #f9f9f9;
 }
 .services__title {
   margin-bottom: 20px;
@@ -219,6 +338,9 @@ main {
   color: #fff;
   text-decoration: none;
 }
+.service-item__link a:hover {
+  color: var(--primary-color);
+}
 /* Equipment */
 .equipment {
   position: relative;
@@ -257,6 +379,7 @@ main {
   position: relative;
   flex: 1;
   padding: 15px;
+  background-color: #fff;
   border-radius: 10px;
   box-shadow: 0 2px 10px rgba(0, 0, 0, 0.4);
 }
@@ -305,5 +428,96 @@ main {
 }
 .brands__list li img {
   max-width: 100%;
+}
+/* Why */
+.why {
+  margin-bottom: 80px;
+}
+.why__content {
+  padding: 40px;
+  background-color: var(--primary-color);
+  border-radius: 10px;
+}
+.why__title {
+  margin-bottom: 40px;
+  font-size: 20px;
+  font-weight: 500;
+  text-align: center;
+  color: #fff;
+}
+.why__list {
+  display: flex;
+  gap: 80px;
+}
+/* Why Item */
+.why-item {
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
+}
+.why-item__content {
+  display: flex;
+  flex-direction: column;
+}
+.why-item__title {
+  margin-top: 4px;
+  margin-bottom: 20px;
+  font-size: 16px;
+  font-weight: 500;
+  color: #fff;
+}
+.why-item__description {
+  font-size: 12px;
+  font-weight: 500;
+  color: #bcffff;
+}
+/* News */
+.news__title {
+  position: relative;
+  margin-bottom: 80px;
+  padding-bottom: 10px;
+  font-size: 20px;
+  font-weight: 500;
+  text-align: center;
+}
+.news__title::before {
+  content: "";
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  width: 50px;
+  height: 3px;
+  margin: 0 auto;
+  background-color: var(--primary-color);
+}
+.news__list {
+  display: flex;
+  gap: 20px;
+}
+/* News Item */
+.news-item__image {
+  margin-bottom: 20px;
+}
+.news-item__title {
+  margin-bottom: 20px;
+  font-weight: 500;
+}
+.news-item__description {
+  margin-bottom: 20px;
+  font-size: 12px;
+  font-weight: 500;
+  color: #6c6c6c;
+}
+.news-item__link {
+  font-size: 12px;
+  font-weight: 700;
+}
+.news-item__link a {
+  color: #000;
+  text-decoration: none;
+}
+.news-item__link a:hover {
+  color: var(--primary-color);
 }
 </style>
